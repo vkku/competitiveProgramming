678dd45e80db049d6992903f5f9b91fa7bbec193
vkku <kumar.vivekten@gmail.com>
2020-05-01 23:55:16 +0530

    List based operations

---
+++ me.vkku.compprog/src/main/java/FarthestCount.java
@@ -0,0 +1,49 @@
+package java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import static java.lang.Math.abs;
+
+public class FarthestCount {
+    public static void farthest(String[] args) throws IOException {
+
+        //BufferedReader
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String name = br.readLine();                // Reading input from STDIN
+
+        //Scanner
+        Scanner s = new Scanner(System.in);
+        String numbers = s.nextLine();                 // Reading input from STDIN
+
+        String[] numArr = numbers.split(" ");
+        List<String> numArrList = new ArrayList<String>(numArr.length);
+        for (String am:numArr) {
+            numArrList.add( am );
+        }
+        List<Integer> numIntList = numArrList.stream().map(Integer::parseInt).collect(Collectors.toList());
+
+        Collections.sort(numIntList);
+
+        Integer smallest = abs(numIntList.get(0));
+        Integer largest = abs(numIntList.get(numIntList.size()-1));
+
+        Integer distance = largest - smallest > 0 ? largest : smallest;
+
+        if(distance == smallest){
+            System.out.println("-"+smallest);
+        }
+        else {
+            System.out.println(largest);
+        }
+
+    }
+
+
+
+}
---
+++ me.vkku.compprog/src/test/main/java/arrays/FarthestCountTest.java
@@ -0,0 +1,30 @@
+package main.java.arrays;
+
+import org.junit.Test;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class FarthestCountTest {
+
+    @Test
+    public void testUrlEncoder() throws IOException {
+
+
+        //BufferedReader
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String name = br.readLine();                // Reading input from STDIN
+        System.out.println("Hi, " + name + ".");    // Writing output to STDOUT
+
+        //Scanner
+        Scanner s = new Scanner(System.in);
+        String name2 = s.nextLine();                 // Reading input from STDIN
+        System.out.println("Hi, " + name + ".");    // Writing output to STDOUT
+
+
+        // Write your code here
+
+    }
+}
+
